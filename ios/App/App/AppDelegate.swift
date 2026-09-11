0a9a179f1f0a7783d4ac5b6ff92a3677ea7c0efe
Speighty118 <tomspeight118@yahoo.com>
2026-09-11 15:29:54 +0100

    Fix AppDelegate build error: remove invalid notification forward
    capacitorDidReceiveRemoteNotification isn't a real Capacitor
    NSNotification.Name constant - invented it by mistake. The two
    methods that matter (registration success/failure) are Capacitor's
    actual documented requirement and are unaffected.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -57,8 +57,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
     }
 
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        NotificationCenter.default.post(name: .capacitorDidReceiveRemoteNotification, object: userInfo, userInfo: [UIApplication.LaunchOptionsKey.remoteNotification: userInfo, "completionHandler": completionHandler])
-    }
-
 }
